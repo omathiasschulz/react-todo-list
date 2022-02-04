63a73d131033368acc9c4b68d40ce0c66bea705a
Mathias Schulz <mathias@schulz.net.br>
2022-02-04 09:29:07 -0300

    [ADD] #login - adicionado pagina login

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,16 +1,19 @@
-import { Navigate } from "react-router-dom";
-import { BrowserRouter, Route, Routes as Switch } from "react-router-dom";
-import { Dashboard } from "../pages";
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes as Switch,
+} from "react-router-dom";
+import { Dashboard, Login } from "../pages";
 
 export const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
-
-        <Route path='/pagina-inicial' element={<Dashboard/>} />
+        <Route path='/entrar' element={<Login />} />
+        <Route path='/pagina-inicial' element={<Dashboard />} />
 
         <Route path='*' element={<Navigate to='/pagina-inicial' />} />
-
       </Switch>
     </BrowserRouter>
   );
