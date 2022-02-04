e2712ee98a54123bacd21de9b1a44eb02ff64144
Mathias Schulz <mathias@schulz.net.br>
2022-02-04 09:39:55 -0300

    [ADD] #form-login - adicionado form para login

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
 
 export const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEntrar = () => {
+    console.log(email);
+    console.log(password);
+  }
+
   return (
     <div>
-      <p>Login</p>
+      <form>
+        <label>
+          <span>Email</span>
+          <input value={email} onChange={e => setEmail(e.target.value)}></input>
+        </label>
+
+        <label>
+          <span>Senha</span>
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)}></input>
+        </label>
+
+        <button type="button" onClick={handleEntrar}>
+          Entrar
+        </button>
+      </form>
     </div>
   );
 };
