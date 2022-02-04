7c87b9226fd8492cb85ee690196dda4049b02c3f
Mathias Schulz <mathias@schulz.net.br>
2022-02-04 11:44:28 -0300

    [ADD] #contador - adicionado contador de clicks na tela principal

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,21 @@
+import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 export const Dashboard = () => {
+  const counterRef = useRef({ counter: 0 });
+
   return (
     <div>
       <br></br>
       <p>Dashboard</p>
+
+      <p>Contador: {counterRef.current.counter}</p>
+      <button onClick={() => counterRef.current.counter++}>Incrementar</button>
+      <button onClick={() => console.log(counterRef.current.counter)}>Apresenta Valor</button>
+
+      <br></br>
+      <br></br>
+
       <Link to='/entrar'>Login</Link>
     </div>
   );
