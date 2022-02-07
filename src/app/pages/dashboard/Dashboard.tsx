71b3ce1472a386814868f805cbee706503cbe389
Mathias Schulz <mathias@schulz.net.br>
2022-02-07 10:45:10 -0300

    [ADD] #todo-list - dashboard todo list

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useUsuarioLogado } from '../../shared/hooks';
 
@@ -6,6 +6,19 @@ export const Dashboard = () => {
   const counterRef = useRef({ counter: 0 });
 
   const { nomeDoUsuario, logout } = useUsuarioLogado();
+
+  const [lista, setLista] = useState<string[]>(['Todo 01', 'Todo 02', 'Todo 03']);
+
+  const handleInputKeyDown:React.KeyboardEventHandler<HTMLInputElement> = useCallback((e) => {
+    const value = e.currentTarget.value.trim();
+    if (e.key !== 'Enter' || value.length === 0) return;
+
+    e.currentTarget.value = '';
+    setLista((oldLista) => {
+      if (oldLista.includes(value)) return oldLista;
+      return [...oldLista, value]
+    });
+  }, []);
 
   return (
     <div>
@@ -23,6 +36,20 @@ export const Dashboard = () => {
       <br></br>
 
       <Link to='/entrar'>Login</Link>
+
+      <br></br>
+      <br></br>
+
+      <h3>Todo List</h3>
+
+      <input onKeyDown={handleInputKeyDown} />
+
+      <br></br>
+      <br></br>
+
+      <ul>
+        {lista.map((item) => <li key={item}>{item}</li>)}
+      </ul>
     </div>
   );
 };
