190df9af9cdc356d0fa31e6aeb6d199396d955a5
Mathias Schulz <mathias@schulz.net.br>
2022-02-07 09:35:33 -0300

    [ADD] #state - state para setar nome usuario logado

--- src/app/shared/contexts/UsuarioLogado.tsx
+++ src/app/shared/contexts/UsuarioLogado.tsx
@@ -1,4 +1,4 @@
-import { createContext, useCallback } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 
 interface IUsuarioLogadoContextData {
   nomeDoUsuario: string;
@@ -7,12 +7,19 @@ interface IUsuarioLogadoContextData {
 export const UsuarioLogadoContext = createContext<IUsuarioLogadoContextData>({} as IUsuarioLogadoContextData);
 
 export const UsuarioLogadoProvider: React.FC = ({ children }) => {
+  const [nome, setNome] = useState('');
+  useEffect(() => {
+    setTimeout(() => {
+      setNome('Mathias Artur Schulz');
+    }, 1000);
+  });
+
   const handleLogout = useCallback(() => {
     console.log('Logout realizado!');
   }, []);
 
   return (
-    <UsuarioLogadoContext.Provider value={{ nomeDoUsuario: 'Mathias Artur Schulz', logout: handleLogout}}>
+    <UsuarioLogadoContext.Provider value={{ nomeDoUsuario: nome, logout: handleLogout}}>
       {children}
     </UsuarioLogadoContext.Provider>
   );
